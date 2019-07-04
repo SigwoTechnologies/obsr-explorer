d7115ff0850b0100ebdaff8b691d543094ba329a
sigwo <steven@sigwo.com>
2019-07-04 13:46:31 -0500

    masternode rpc call fix, workaround crons

--- cron/masternode.js
+++ cron/masternode.js
@@ -22,7 +22,7 @@ async function syncMasternode() {
   // Increase the timeout for masternode.
   rpc.timeout(10000); // 10 secs
 
-  const mns = await rpc.call('masternode', ['list']);
+  const mns = await rpc.call('listmasternodes', ['list']);
   const inserts = [];
   await forEach(mns, async (mn) => {
     const masternode = new Masternode({
