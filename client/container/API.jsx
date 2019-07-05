da45817a9e9f4165e1536b94c614b7b6e208fd2c
EliteBook <kylehensman@gmail.com>
2019-07-05 12:34:21 -0300

    Updated input styles and added links to section of the page

--- client/container/API.jsx
+++ client/container/API.jsx
@@ -4,16 +4,25 @@ import React from 'react';
 
 import APIdata from '../component/API/APIdata';
 import APIsection from '../component/API/APIsection';
+import APIname from '../component/API/APIname';
 
 export default class API extends Component {
 
   getSections = () => {
     return APIdata.map((section, idx) => {
       return <APIsection
-        key={ idx }
-        heading={ section.heading }
-        subHeading={ section.subHeading }
-        calls={ section.calls } />
+        key={idx}
+        heading={section.heading}
+        subHeading={section.subHeading}
+        calls={section.calls} />
+    });
+  };
+
+  getSectionsNames = () => {
+    return APIdata.map((section, idx) => {
+      return <APIname
+        key={idx}
+        calls={section.calls} />
     });
   };
 
@@ -23,8 +32,10 @@ export default class API extends Component {
         <div className="row">
           <div className="col-md-3">
             <div className="api__nav">
-              API Calls <br />
+              {this.getSectionsNames()}
+              {/* API Calls <br />
               <p className="api__section-sub-heading">
+
                 getAddress [hash]<br />
                 getBlock [hash][height]<br />
                 getBlockAverage<br />
@@ -57,14 +68,14 @@ export default class API extends Component {
                 Transaction (/#/tx/[hash])<br />
                 Block (/#/block/[hash|height]<br />
                 Address (/#/address/[hash]<br />
-              </p>
+              </p> */}
             </div>
           </div>
           <div className="col-md-9">
-          <p className="api__section-sub-heading">The block explorer provides and API 
-        allowing users and/or applications to retrieve information from the network 
+            <p className="api__section-sub-heading">The block explorer provides and API
+          allowing users and/or applications to retrieve information from the network
         without the need for a local wallet.</p><br /><br />
-            { this.getSections() }
+            {this.getSections()}
           </div>
         </div>
       </div>
